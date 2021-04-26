b3b41bc87f62dcc642e7fd6da298e719c6c82611
jujuzitroni <jujuzitroni@gmail.com>
2021-04-26 16:16:48 +0200

    add js to new page

---
+++ newTask.js
@@ -0,0 +1,9 @@
+// alert("testtest!!");
+
+const submitButton = document.querySelector(".form__submit");
+
+submitButton.addEventListener("click", addTask);
+
+function addTask() {
+  alert("show task");
+}
