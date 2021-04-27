0d51b8eb326ba11c5a740b7928852d6701d427c4
jujuzitroni <jujuzitroni@gmail.com>
2021-04-27 17:06:36 +0200

    add if statement

--- newTask.js
+++ newTask.js
@@ -1,10 +1,29 @@
 const taskNameInput = document.querySelector(".text-input");
 const formElement = document.querySelector(".form");
 
+// formElement.onsubmit = function (event) {
+//   event.preventDefault();
+//   const checkedDateSelector = document.querySelector(
+//     ".radio-group__input:checked"
+//   );
+//   console.log(taskNameInput.value, checkedDateSelector.value);
+// };
+
 formElement.onsubmit = function (event) {
   event.preventDefault();
   const checkedDateSelector = document.querySelector(
     ".radio-group__input:checked"
   );
+
+  if (taskNameInput.value === "") {
+    alert("Please enter a Task");
+    return;
+  }
+
+  if (checkedDateSelector === null) {
+    alert("Please select a Date");
+    return;
+  }
+
   console.log(taskNameInput.value, checkedDateSelector.value);
 };
