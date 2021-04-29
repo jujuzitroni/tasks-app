31ceeee8ada1c14917dd2f178e44f6adff51861e
jujuzitroni <jujuzitroni@gmail.com>
2021-04-29 16:11:57 +0200

    add createTaskElement function

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,19 @@
-alert("hello");
+function createTaskElement(taskName) {
+  const label = document.createElement("label");
+  const input = document.createElement("input");
+  const span = document.createElement("span");
+
+  input.type = "checkbox";
+  input.className = "checkbox-group__input";
+  span.innerText = taskName;
+
+  label.append(input, span);
+  return label;
+}
+
+const brewTeaElement = createTaskElement("Tee kochen");
+const drinkTeaElement = createTaskElement("Tee trinken");
+
+const taskGroupElement = document.querySelector(".checkbox-group");
+
+taskGroupElement.append(brewTeaElement, drinkTeaElement);
